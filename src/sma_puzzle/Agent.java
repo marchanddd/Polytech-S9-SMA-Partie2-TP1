9300debccce30db8fddfe396cfb7e95870740c51
QuentinChevillot <quentin.chevillot@gmail.com>
2015-01-27 08:55:27 +0100

    premier commit

---
+++ src/sma_puzzle/Agent.java
@@ -0,0 +1,92 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package sma_puzzle;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Epulapp
+ */
+public class Agent extends Thread {
+    private int[][] position;
+    private Grille grille; //grille
+    private int[][] positionFinale;//position finale
+    private ArrayList<Message> msgTraites;//liste des messages traités
+    private ArrayList<Message> msgEnvoyes;//liste des messages envoyés
+    private ArrayList<ArrayList<Message>> mailBox;
+    
+    public Agent(){
+        
+    }
+    public Agent(int[][] p,Grille g, int[][] pf, ArrayList<ArrayList<Message>> mb){
+        position = p;
+        grille = g;
+        positionFinale = pf;
+        mailBox = mb;
+        
+    }
+    
+
+    public int[][] getPosition() {
+        return position;
+    }
+
+    public Grille getGrille() {
+        return grille;
+    }
+
+    public int[][] getPositionFinale() {
+        return positionFinale;
+    }
+
+    public Message[] getMsgTraites() {
+        return msgTraites;
+    }
+
+    public Message[] getMsgEnvoyes() {
+        return msgEnvoyes;
+    }
+
+    public ArrayList<ArrayList<Message>> getMailBox() {
+        return mailBox;
+    }
+    
+    public void setPosition(int[][] position) {
+        this.position = position;
+    }
+
+    public void setGrille(Grille grille) {
+        this.grille = grille;
+    }
+
+    public void setPositionFinale(int[][] positionFinale) {
+        this.positionFinale = positionFinale;
+    }
+
+    public void setMsgTraites(Message[] msgTraites) {
+        this.msgTraites = msgTraites;
+    }
+
+    public void setMsgEnvoyes(Message[] msgEnvoyes) {
+        this.msgEnvoyes = msgEnvoyes;
+    }
+
+    public void setMailBox(ArrayList<ArrayList<Message>> mailBox) {
+        this.mailBox = mailBox;
+    }
+    
+    
+    
+    //tant que le puzzle n'est pas reconstitué on boucle 
+    //autre solution : gain individuel failbe si on est dans la bonne position et meilleur gain si tout le monde y est.
+    
+        //regarder si on est en position finale
+        //consulter ses messages et les traiter
+
+    
+}
---
+++ src/sma_puzzle/Message.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package sma_puzzle;
+
+/**
+ *
+ * @author Epulapp
+ */
+public class Message {
+    private Agent expediteur;
+    private Agent destinataire;
+    private String actLang;
+    private String action;
+    private int[][] position;//position
+}
