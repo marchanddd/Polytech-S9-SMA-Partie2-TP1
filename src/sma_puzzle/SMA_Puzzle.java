d8c43093d35db215c5242c6a7e88602af3037f69
QuentinChevillot <quentin.chevillot@gmail.com>
2015-01-27 09:19:13 +0100

    rzere

--- src/sma_puzzle/SMA_Puzzle.java
+++ src/sma_puzzle/SMA_Puzzle.java
@@ -16,7 +16,7 @@ public class SMA_Puzzle {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Agent agt1 = new Agent
     }
     
 }
